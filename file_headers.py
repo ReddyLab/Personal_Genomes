c239869a8d61eee2c594df7f7c4aae4cd2a55719
katherinedura <katherinedura@gmail.com>
2025-03-31 11:09:24 -0400

    Added file headers

---
+++ file_headers.py
@@ -0,0 +1,46 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Jun 13 09:31:31 2023
+
+@author: kd259
+"""
+
+def vcf_bowtie_hg38():
+    header = ""
+
+
+    return header
+
+
+def sam_bowtie_hg38():
+    header = "@HD\tVN:1.3\tSO:coordinate\n"
+    header+= "@SQ\tSN:chr1\tLN:248956422\n"
+    header+= "@SQ\tSN:chr2\tLN:242193529\n"
+    header+= "@SQ\tSN:chr3\tLN:198295559\n"
+    header+= "@SQ\tSN:chr4\tLN:190214555\n"
+    header+= "@SQ\tSN:chr5\tLN:181538259\n"
+    header+= "@SQ\tSN:chr6\tLN:170805979\n"
+    header+= "@SQ\tSN:chr7\tLN:159345973\n"
+    header+= "@SQ\tSN:chr8\tLN:145138636\n"
+    header+= "@SQ\tSN:chr9\tLN:138394717\n"
+    header+= "@SQ\tSN:chr10\tLN:133797422\n"
+    header+= "@SQ\tSN:chr11\tLN:135086622\n"
+    header+= "@SQ\tSN:chr12\tLN:133275309\n"
+    header+= "@SQ\tSN:chr13\tLN:114364328\n"
+    header+= "@SQ\tSN:chr14\tLN:107043718\n"
+    header+= "@SQ\tSN:chr15\tLN:101991189\n"
+    header+= "@SQ\tSN:chr16\tLN:90338345\n"
+    header+= "@SQ\tSN:chr17\tLN:83257441\n"
+    header+= "@SQ\tSN:chr18\tLN:80373285\n"
+    header+= "@SQ\tSN:chr19\tLN:58617616\n"
+    header+= "@SQ\tSN:chr20\tLN:64444167\n"
+    header+= "@SQ\tSN:chr21\tLN:46709983\n"
+    header+= "@SQ\tSN:chr22\tLN:50818468\n"
+    header+= "@SQ\tSN:chrX\tLN:156040895\n"
+    header+= "@SQ\tSN:chrY\tLN:57227415\n"
+    header+= "@SQ\tSN:chrM\tLN:16569\n"
+
+
+    return header
+
